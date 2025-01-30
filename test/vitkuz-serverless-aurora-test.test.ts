5e556c4d2949119f91829dc6b5b1cfb92d806dd8
Vitali Kuzmenka <vitkuz.info@gmail.com>
2025-01-30 16:09:15 +0400

    Initial commit

---
+++ test/vitkuz-serverless-aurora-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VitkuzServerlessAuroraTest from '../lib/vitkuz-serverless-aurora-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vitkuz-serverless-aurora-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VitkuzServerlessAuroraTest.VitkuzServerlessAuroraTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
